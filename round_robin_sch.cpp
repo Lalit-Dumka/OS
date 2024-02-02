f64b2b59366a359ba4b024cbbc4b9c7aea932c57
Lalit Dumka <lalitdumka128@gmail.com>
2024-02-02 07:26:33 +0530

    edited

--- round_robin_sch.cpp
+++ round_robin_sch.cpp
@@ -1,52 +1,72 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
 
-struct rr {
-    int pid, bt, sbt, wt, lst;
-} p[10];
+struct Process {
+    int processNumber, burstTime, remainingBurstTime, waitingTime, lastScheduledTime;
+} processes[10];
 
 int main() {
-    int pp = -1, ts, flag, count, ptm = 0, i, n, twt = 0, ttat = 0;
-    cout << "Enter the number of processes: ";
-    cin >> n;
-    cout << "Enter the time slice: ";
-    cin >> ts;
-    for (i = 0; i < n; i++) {
-        cout << "Enter the burst time of process " << i + 1 << ": ";
-        cin >> p[i].bt;
-        p[i].sbt = p[i].bt;
-        p[i].pid = i + 1;
-        p[i].wt = p[i].lst = 0;
+    int previousProcessIndex = -1, timeSlice, flag, count, currentTime = 0, i, numProcesses, totalWaitingTime = 0, totalTurnaroundTime = 0;
+
+    printf("\nRound Robin Scheduling............");
+    printf("\nEnter the number of processes:");
+    scanf("%d", &numProcesses);
+
+    printf("Enter the time slice:");
+    scanf("%d", &timeSlice);
+
+    printf("Enter the burst time\n");
+    for (i = 0; i < numProcesses; i++) {
+        printf("Process %d: ", i + 1);
+        scanf("%d", &processes[i].burstTime);
+        processes[i].waitingTime = processes[i].lastScheduledTime = 0;
+        processes[i].processNumber = i + 1;
+        processes[i].remainingBurstTime = processes[i].burstTime;
     }
-    while (1) {
+
+    printf("Scheduling....\n");
+
+    do {
         flag = 0;
-        for (i = 0; i < n; i++) {
-            if (p[i].sbt > 0) {
-                flag = 1;
-                if (p[i].sbt > ts) {
-                    p[i].sbt -= ts;
-                    ptm += ts;
-                }
-                else {
-                    ptm += p[i].sbt;
-                    p[i].sbt = 0;
-                    p[i].wt = ptm - p[i].bt;
+        for (i = 0; i < numProcesses; i++) {
+            count = processes[i].remainingBurstTime;
+            if (count > 0) {
+                flag = -1;
+                count = (count >= timeSlice) ? timeSlice : count;
+
+                printf("\nProcess %d", processes[i].processNumber);
+                printf(" from %d", currentTime);
+                currentTime += count;
+                printf(" to %d", currentTime);
+
+                processes[i].remainingBurstTime -= count;
+
+                if (previousProcessIndex != i) {
+                    previousProcessIndex = i;
+                    processes[i].waitingTime += currentTime - processes[i].lastScheduledTime - count;
+                    processes[i].lastScheduledTime = currentTime;
                 }
             }
         }
-        if (flag == 0)
-            break;
+
+        if (flag == 0) {
+            break;  // All processes completed their execution
+        }
+
+    } while (1);
+
+    printf("\n\nProcess\tBurst Time\tWaiting Time\tTurnaround Time");
+
+    for (i = 0; i < numProcesses; i++) {
+        totalWaitingTime += processes[i].waitingTime;
+        printf("\n%d\t\t%d\t\t%d\t\t%d", processes[i].processNumber, processes[i].burstTime, processes[i].waitingTime, processes[i].waitingTime + processes[i].burstTime);
     }
 
-    for (i = 0; i < n; i++) {
-        int tat = p[i].bt + p[i].wt;
-        ttat += tat;
-        twt += p[i].wt;
-        cout << "Process " << p[i].pid << " - Turnaround Time: " << tat << ", Waiting Time: " << p[i].wt << endl;
-    }
-
-    cout << "Average Turnaround Time: " << (float)ttat / n << endl;
-    cout << "Average Waiting Time: " << (float)twt / n << endl;
+    totalTurnaroundTime = currentTime;
+    printf("\n\nTotal Waiting Time: %d", totalWaitingTime);
+    printf("\nAverage Waiting Time: %f", (float)totalWaitingTime / numProcesses);
+    printf("\nTotal Turnaround Time: %d", totalTurnaroundTime);
+    printf("\nAverage Turnaround Time: %f\n", (float)totalTurnaroundTime / numProcesses);
 
     return 0;
 }
